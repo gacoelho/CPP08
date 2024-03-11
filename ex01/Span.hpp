8a2db7f102e47cf9f1e3f1b0f6142f59d641bff7
gacoelho <gtorres2003@gmail.com>
2024-03-11 15:50:43 -0300

    ex01 adde n finished;

---
+++ ex01/Span.hpp
@@ -0,0 +1,22 @@
+#ifndef SPAN_HPP
+# define SPAN_HPP
+# include <vector>
+# include <iostream>
+# include <algorithm>
+
+class Span
+{
+    private:
+        std::vector<int> _v;
+        unsigned int  _n;
+    public:
+        Span(unsigned int n);
+        Span(const Span & copy);
+        ~Span();
+        Span &operator=(const Span & copy);
+        void addNumber(int n);
+        int shortestSpan();
+        int longestSpan();
+        void addRange(std::vector<int>::iterator begin, std::vector<int>::iterator end);
+};
+#endif
